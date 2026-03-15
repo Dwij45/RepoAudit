8d392b33a3fd68c546619462daafef67fbe78ae0
Dwij <gosaidwij045@gmail.com>
2026-03-16 01:25:41 +0530

    schema update migration

---
+++ backend/prisma/migrations/20260315193530_filepath_update/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `projectPath` on the `ScanResult` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ScanResult" DROP COLUMN "projectPath";
