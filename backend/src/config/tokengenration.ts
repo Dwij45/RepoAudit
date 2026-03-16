76036a636d484c6263d8f70562dbe4277d54bfdc
Dwij <gosaidwij045@gmail.com>
2026-03-16 13:46:33 +0530

    add config for token generation

--- backend/src/config/tokengenration.ts
+++ backend/src/config/tokengenration.ts
@@ -5,14 +5,12 @@ import jwt from "jsonwebtoken";
 export const generateToken = (userId: number, res: Response) => {
   const token = jwt.sign(
     { userId },
-    process.env.JWT_SECRET as string,
-    { expiresIn: "7d" }
+    process.env.JWT_SECRET as string
   );
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: false, // true in production (HTTPS)
-    sameSite: "lax",
-    maxAge: 7 * 24 * 60 * 60 * 1000
+    secure: false,
+    sameSite: "lax"
   });
 };
