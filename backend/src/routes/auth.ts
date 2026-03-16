bfb763bddb551efdbc420692b27d16025cfb66cd
Dwij <gosaidwij045@gmail.com>
2026-03-16 13:25:21 +0530

    auth route update

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import {signup, login } from "../controllers/authController.js";
+import {signup, login,logout,updateProfile, Profile } from "../controllers/authController.js";
 import express from "express";
+
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const authRouter=Router();
 authRouter.use(express.json());
 authRouter.post("/signup",signup);
 authRouter.post("/login",login);
+authRouter.post("/logout",logout);
+authRouter.put("/profile",authMiddleware,updateProfile);
+authRouter.get("/profile",authMiddleware,Profile);
 
 export default authRouter;
